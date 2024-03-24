ff4de64162a3ac21ea7842c2ae869f07732865d6
vladutpodaru <vladut.podaru97@gmail.com>
2024-03-24 20:43:51 +0200

    First commit Adding folders + arhitecture gitignore

---
+++ JourneyHub/WebAPI/Program.cs
@@ -0,0 +1,31 @@
+using Application;
+using Infrastructure;
+using Presentation;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+// Logger: TODO: to put configuration in appsettings
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+app.Run();

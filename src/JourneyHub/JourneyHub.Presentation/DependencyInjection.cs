b8b26cdb5173415ed51565d301914e4408c04e85
vladutpodaru <52547346+vladutpodaru@users.noreply.github.com>
2024-04-11 18:56:19 +0300

    DI + libraries

---
+++ src/JourneyHub/JourneyHub.Presentation/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using HotChocolate.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JourneyHub.Presentation
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddGraphQLServer()
+                    .AddQueryType()
+                    .AddMutationType()
+                    .AddSubscriptionType();
+
+            return services;
+        }
+    }
+}
